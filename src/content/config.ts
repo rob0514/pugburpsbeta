9be88e0809f0b1266bda6ee347a902580b66c9a3
Rob <babblespring@gmail.com>
2023-03-25 15:22:18 -1000

    config

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,28 +10,7 @@ const blogCollection = defineCollection({
         categories: z.array(z.string()),
     }),
 })
-const authorCollection = defineCollection({
-    schema: z.object({
-        name: z.string(),
-        email: z.string(),
-        thumbnail: z.string().optional(),
-        bio: z.string(),
-        layout: z.string().optional(),
-    }),
-})
-const productCollection = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        date: z.string().transform((str) => new Date(str)),
-        thumbnail: z.string().optional(),
-        headerText: z.string(),
-        layout: z.string().optional(),
-        categories: z.array(z.string()),
-    }),
-})
 
 export const collections = {
-    products: productCollection,
     posts: blogCollection,
-    authors: authorCollection,
 }
