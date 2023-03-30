10e4ad538f59e51e52653231e28c4e1eb6c2d916
Rob <babblespring@gmail.com>
2023-03-30 07:54:41 -1000

    Big Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    root: './',
-    srcDir: './src',
-    publicDir: './public',
-    outDir: './dist',
-    assets: 'assets',
-    integrations: [tailwind(
-      {
-        config: { applyBaseStyles: false },
-      }
-  )]
+  root: './',
+  srcDir: './src',
+  publicDir: './public',
+  outDir: './dist',
+  assets: 'assets',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), alpinejs()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			aspectRatio: {
+				'golden': '1.618 / 1',
+				'rev-golden': '247 / 371'
+			  },
 			fontFamily: {
 				'sans': ['Plus Jakarta Sans','Helvetica', 'Arial', 'sans-serif'],
 				'serif': ['Tiempos','Helvetica', 'Arial', 'sans-serif'],
